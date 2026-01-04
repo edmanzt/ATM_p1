ca6d1cf6c9ecdc56d045903d0907ce97b322e02c
edmanzt <edmanzardosht@gmail.com>
2026-01-05 00:10:58 +0330

    Update Bank(ATMp-1).c

--- Bank(ATMp-1).c
+++ Bank(ATMp-1).c
@@ -14,13 +14,15 @@ int main() {
 
     int enteredPin, tries;
 
-    // --- bakhsh vorood ramz va control tedad talash ha ---
+    // --- bakhsh vorood ramz va kontrol tedad talash ha ---
     for (tries = 0; tries < 3; tries++) {
-        printf("Ramz ra vared konid: ");
+        printf("ramz ra vared konid: ");
         scanf("%d", &enteredPin);
 
-        if (enteredPin == pin) break;
-        printf("Ramz eshtebah!\n");
+        if (enteredPin == pin) {
+            break;
+        }
+        printf("ramz eshtebah!\n");
     }
 
     if (tries == 3) {
@@ -32,23 +34,26 @@ int main() {
     do {
         // --- bakhsh namayesh menu asli ---
         printf("\n--- Menu ---\n");
-        printf("1. Create account\n");
-        printf("2. Deposit\n");
-        printf("3. Withdraw\n");
-        printf("4. Change PIN\n");
-        printf("5. Exit\n");
+        printf("1. ijad hesab\n");
+        printf("2. Variz\n");
+        printf("3. Bardasht\n");
+        printf("4. Namayesh mojoodi\n");
+        printf("5. Namayesh tarakonesh ha\n");
+        printf("6. Taghir ramz\n");
+        printf("7. Hazf hesab\n");
+        printf("8. Khorooj\n");
         printf("Entekhab shoma: ");
         scanf("%d", &choice);
 
         // --- bakhsh ijad hesab ---
         if (choice == 1) {
-            if (isActive) {
-                printf("Hesab ghablan ijad shode!\n");
+            if (isActive == 1) {
+                printf("Hesab ghablan sakhte shode!\n");
             } else {
-                printf("Nam khod ra vared konid: ");
+                printf("nam khod ra vared konid: ");
                 scanf("%s", firstName);
 
-                printf("Nam khanevadegi ra vared konid: ");
+                printf("nam khanevadegi khod vared konid: ");
                 scanf("%s", lastName);
 
                 printf("Mojoodi avalie ra vared konid: ");
@@ -56,13 +61,12 @@ int main() {
 
                 isActive = 1;
                 transactionCount = 0;
-                printf("Hesab ba movafaghiat ijad shod!\n");
+                printf("Hesab ba movafaghiat sakhte shod!\n");
             }
         }
-
         // --- bakhsh variz ---
         else if (choice == 2) {
-            if (!isActive) {
+            if (isActive == 0) {
                 printf("Hesab faal nist!\n");
             } else {
                 int amount;
@@ -71,10 +75,14 @@ int main() {
 
                 if (amount > 0) {
                     balance += amount;
+
                     if (transactionCount < 10) {
-                        transactions[transactionCount++] = amount;
+                        transactions[transactionCount] = amount;
+                        transactionCount++;
                     } else {
-                        for (int i = 1; i < 10; i++) transactions[i-1] = transactions[i];
+                        for (int i = 1; i < 10; i++) {
+                            transactions[i-1] = transactions[i];
+                        }
                         transactions[9] = amount;
                     }
                     printf("Variz ba movafaghiat anjam shod!\n");
@@ -83,10 +91,9 @@ int main() {
                 }
             }
         }
-
         // --- bakhsh bardasht ---
         else if (choice == 3) {
-            if (!isActive) {
+            if (isActive == 0) {
                 printf("Hesab faal nist!\n");
             } else {
                 int amount;
@@ -95,10 +102,14 @@ int main() {
 
                 if (amount > 0 && amount <= balance) {
                     balance -= amount;
+
                     if (transactionCount < 10) {
-                        transactions[transactionCount++] = -amount;
+                        transactions[transactionCount] = -amount;
+                        transactionCount++;
                     } else {
-                        for (int i = 1; i < 10; i++) transactions[i-1] = transactions[i];
+                        for (int i = 1; i < 10; i++) {
+                            transactions[i-1] = transactions[i];
+                        }
                         transactions[9] = -amount;
                     }
                     printf("Bardasht ba movafaghiat anjam shod!\n");
@@ -107,33 +118,64 @@ int main() {
                 }
             }
         }
-
-        // --- bakhsh taghir ramz ---
+        // --- bakhsh namayesh mojoodi ---
         else if (choice == 4) {
+            if (isActive == 0) {
+                printf("Hesab faal nist!\n");
+            } else {
+                printf("Mojoodi kononi: %d\n", balance);
+            }
+        }
+        // --- bakhsh namayesh tarakonesh ha ---
+        else if (choice == 5) {
+            if (isActive == 0) {
+                printf("Hesab faal nist!\n");
+            } else {
+                printf("tarakonesh haye akhir:\n");
+                for (int i = 0; i < transactionCount; i++) {
+                    printf("%d\n", transactions[i]);
+                }
+            }
+        }
+
+        // --- taghir ramz ---
+        else if (choice == 6) {
             int oldPin, newPin;
-            printf("Ramz feli ra vared konid: ");
+            printf("ramz feli ra vared konid: ");
             scanf("%d", &oldPin);
 
             if (oldPin == pin) {
-                printf("Ramz jadid ra vared konid: ");
+                printf("ramz jadid ra vared konid: ");
                 scanf("%d", &newPin);
                 pin = newPin;
-                printf("Ramz ba movafaghiat taghir kard!\n");
+                printf("ramz ba movafaghiat taghir kard!\n");
             } else {
-                printf("Ramz eshtebah ast!\n");
+                printf("ramz eshtebah ast!\n");
+            }
+        }
+
+        // --- bakhsh hazf hesab ---
+        else if (choice == 7) {
+            if (isActive == 0) {
+                printf("Hesab faal nist!\n");
+            } else {
+                isActive = 0;
+                balance = 0;
+                transactionCount = 0;
+                printf("Hesab hazf shod!\n");
             }
         }
 
         // --- khorooj ---
-        else if (choice == 5) {
-            printf("Bedrood!\n");
+        else if (choice == 8) {
+            printf("bedrood!\n");
         }
 
         else {
             printf("Entekhab na motabar!\n");
         }
 
-    } while (choice != 5);
+    } while (choice != 8);
 
     return 0;
 }
